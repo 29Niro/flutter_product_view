6fca23a4df247f00b15c5c61f13e6fbe3d84efad
29Niro <29niro@gmail.com>
2022-12-10 14:15:05 +0530

    API connected with UI

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_product_view/models/product_model.dart';
 import 'package:flutter_product_view/screens/details_page.dart';
+import 'package:flutter_product_view/services/api_services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +11,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late List<ProductModel>? _productModel = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getProductDetails();
+  }
+
+  void _getProductDetails() async {
+    _productModel = await ApiServices().getProductList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,76 +30,82 @@ class _HomePageState extends State<HomePage> {
         title: const Text("Home"),
         leading: const BackButton(color: Colors.white),
       ),
-      body: Container(
-        height: MediaQuery.of(context).size.height / 4,
-        margin: const EdgeInsets.only(top: 5, left: 5, right: 5),
-        child: Card(
-          elevation: 4,
-          clipBehavior: Clip.antiAlias,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(4.0)),
-          child: Row(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width / 2,
-                height: 150,
-                decoration: const BoxDecoration(
-                    image:
-                        DecorationImage(image: AssetImage('images/dummy.jpg'))),
-              ),
-              const SizedBox(
-                width: 15,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView.builder(
+        itemCount: _productModel!.length,
+        itemBuilder: (context, index) {
+          return Container(
+            height: MediaQuery.of(context).size.height / 4,
+            margin: const EdgeInsets.only(top: 5, left: 5, right: 5),
+            child: Card(
+              elevation: 4,
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4.0)),
+              child: Row(
                 children: [
-                  const Text(
-                    'Title',
-                    style: TextStyle(
-                        overflow: TextOverflow.ellipsis,
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.w700),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                      image: NetworkImage(_productModel![index].thumbnail),
+                    )),
                   ),
                   const SizedBox(
-                    height: 5.0,
+                    width: 15,
                   ),
-                  const Text(
-                    'Brand',
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                  const SizedBox(
-                    height: 5.0,
-                  ),
-                  const Text(
-                    'Price\$',
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      style:
-                          TextButton.styleFrom(backgroundColor: Colors.green),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const DetailsPage()));
-                      },
-                      child: const Text(
-                        "View >>>",
-                        style: TextStyle(color: Colors.white),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Title',
+                        style: TextStyle(
+                            overflow: TextOverflow.ellipsis,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w700),
                       ),
-                    ),
+                      const SizedBox(
+                        height: 5.0,
+                      ),
+                      const Text(
+                        'Brand',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      const SizedBox(
+                        height: 5.0,
+                      ),
+                      const Text(
+                        'Price\$',
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.w500),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                              backgroundColor: Colors.green),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const DetailsPage()));
+                          },
+                          child: const Text(
+                            "View >>>",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
