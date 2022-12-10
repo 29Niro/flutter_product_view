4f41fcf85a3eba8da427bb96d6daf8f5b91e743e
29Niro <29niro@gmail.com>
2022-12-10 13:33:50 +0530

    details page modified

--- lib/screens/details_page.dart
+++ lib/screens/details_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class DetailsPage extends StatefulWidget {
   const DetailsPage({super.key});
@@ -10,6 +10,47 @@ class DetailsPage extends StatefulWidget {
 class _DetailsPageState extends State<DetailsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Details Page");
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Details Page"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [
+          Image(
+            image: AssetImage('images/dummy.jpg'),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Title',
+            style: TextStyle(fontSize: 25.0),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            'Rating',
+            style: TextStyle(fontSize: 18.0),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            'Description',
+            style: TextStyle(fontSize: 15.0),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            'Price\$',
+            style: TextStyle(color: Colors.red, fontSize: 25.0),
+          ),
+        ],
+      ),
+    );
   }
 }
