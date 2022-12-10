18bdb765243dffd544480eb8ee8dfd063827ff0b
29Niro <29niro@gmail.com>
2022-12-10 18:15:17 +0530

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_product_view/screens/home_page.dart';
 import 'package:flutter_product_view/screens/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_product_view/screens/signup_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
